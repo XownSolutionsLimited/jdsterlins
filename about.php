72381f13ac2aaa45be12d4f94e40fbd3bccbad31
honeyspring <honeyspringworld@gmail.com>
2020-12-15 18:24:28 +0100

    create contact and about-us page

---
+++ about.php
@@ -0,0 +1,112 @@
+<?php include ('header.html')?>  
+	<!--- Categories-->
+	<div class="container-fluid padding">
+		<div class="row  welcome text-center">
+			<div class="col-12">
+				<h1 class="display-3 float-left">About JD Sterlens</h1>
+			</div>
+			<hr>
+		</div>
+	</div>
+
+    <!--- Cards -->
+	<div class="container-fluid padding about-us">
+		<div class="row padding ">
+			
+			<div class="col-sm-4s col-md-4 col-lg-4 ">
+            <img class="card-img" src="img/Printers1.png">
+          
+				<div class="card ">
+            
+					<div class="card-body">
+                    <p> JD Sterlen limited supply and provide high quality, flexible and innovative print services using the latest technologies in an environmentally responsible manner<p>
+					</div>
+				</div>
+            </div>
+            <div class="col-sm-8 col-md-8 col-lg-8  text-align-center">  
+                 
+                <h6  class="display-3 float-right text-red" >Business office Supplies and Print Management</h6>  
+                <div class='col-12 d-flex justify-content-center'>
+            <p style="width:50%">
+            Our aim is to provide you, our client with an efficient service, which is quality assured, using the latest technology available. This gives the client flexibility to reduce the cost of production and where required energy consumption.
+
+We work with you to forecast your work schedule and identify cost savings wherever possible using economies of scale, as well as proposing flexible solutions that allow projects to be carried out in a cost effective manner.
+
+We do this by utilising a combination of tailored systems and software as well as appropriate manufacturing techniques
+</p>
+</div>
+</div>
+        </div>
+        <div class="row padding justify-content-center ">
+			<div class="col-sm-6 col-md-6 col-lg-6 ">
+         
+				<div class="card even">
+
+					<div class="card-body">
+					
+                    <p> We differ from other business office supply companies as we deliver quality service and products</p>
+                    <p>Timeliness and flexibility is of paramount importance to our company</p>
+					</div>
+				</div>
+            </div>
+    </div>  <!--ROW2-->
+    
+    <div class="row padding justify-content-end ">
+			<div class="col-sm-6 col-md-6 col-lg-6 ">
+            <img class="card-img" src="img/Electronics1.png">
+				<div class="card ">
+              
+					<div class="card-body">
+					
+					<p>Our client base include Multinationals, Financial markets, as well as the Publishing, Marketing and Charity sectors.</p>
+					</div>
+				</div>
+			</div>
+   
+        </div>  <!--ROW3-->   
+		<div class="row padding  base-row justify-content-center">
+        <div class="col-12">    
+        <h6  class="display-3 text-red float-right pr-10" >WHO WE ARE</h6>  
+</div>
+<div class="col-12 who-we-are d-flex flex-wrap"> 
+			<div class="col-sm-6 col-md-6 col-lg-3  ">
+				<div class="card even">
+			
+					<div class="card-body">
+					
+                   <p> We are a niche supplier with a consultative approach, working best with those that require an outstanding service and expect to be supported by a dedicated and experienced management team willing to ‘go the extra mile’ when needed most.<p>
+					
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-6 col-md-6 col-lg-3 ">
+				<div class="card odd">
+				
+					<div class="card-body">
+					
+					<p>We understand that your company has specific needs which require flexibility, attention to detail, quality in products and services and timeliness</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-6 col-md-6 col-lg-3 ">
+				<div class="card even">
+		
+					<div class="card-body">
+				
+					<p>We use our knowledge and experience to ensure cost effective ness and timely delivery of your office business needs.</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-6 col-md-6 col-lg-3 ">
+				<div class="card odd">
+			
+					<div class="card-body">
+					
+					<p>We never apply a ‘one size fits all’ approach to our work. We pride ourselves on helping you to identify all office business needs and print service requirements across your business and offer intelligent, individual solutions that deliver outstanding results</p>
+					</div>
+				</div>
+			</div>
+        </div>
+        </div>
+	</div>
+
---
+++ contact.php
@@ -0,0 +1,61 @@
+<?php include ('header.html')?>  
+	<!--- Categories-->
+	<div class="container-fluid padding">
+		<div class="row  welcome text-center">
+			<div class="col-12">
+				<h1 class="display-3 float-left">Contact us</h1>
+			</div>
+			<hr>
+		</div>
+	</div>
+
+    <!--- Cards -->
+	<div class="container-fluid padding contact-us">
+		<div class="row padding ">
+			
+			<div class="col-sm-4s col-md-4 col-lg-4 d-flex justify-content-center">
+            <ul>
+				<li class=" nav-item social">
+					<a class="nav-link " href="" ><i class="icon fab fa-facebook-f"></i></a>
+				  </li>
+				  <li class=" nav-item social">
+					<a class="nav-link " href="" ><i class="icon fab fa-twitter"></i></a>
+				  </li>
+				  <li class=" nav-item social">
+					<a class="nav-link " href="" ><i class="icon fab fa-linkedin-in"></i></a>
+                  </li>
+</ul>
+         
+            </div>
+            <div class="col-sm-8 col-md-8 col-lg-8  text-align-center">  
+                 
+                <h6  class="display-3 float-right text-red mb-4" >We Love To Hear From You</h6>  
+                <div class='col-12 mt-4'>
+         
+            
+<form>
+<div class="form-group">
+    <label for="fullName">Full Name</label>
+    <input type="fullName" class="form-control" id="fullName"  placeholder="Full Name">
+    
+  </div>
+
+
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="Message">Message</label>
+    <input type="text-area" class="form-control" id="Message" placeholder="Message">
+  </div>
+  <div class="form-check">
+  
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+</div>
+</div>
+        </div>
+</div>
