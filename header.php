4eb86bc6ff697ddb128b0e4662b6478a0505b4a3
Tobi <37334031+isaactobiloba@users.noreply.github.com>
2020-12-17 18:35:46 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>JD sterlen</title>
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+    <link href="style.css" rel="stylesheet" />
+  </head>
+  <body>
+	
+	
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-md navbar-light bg-light sticky-top bg-white">
+	
+      <div class="container-fluid d-flex flex-wrap" >
+	
+		    <!----brand and search---->
+		<div class='d-flex flex-wrap col-12 head'>
+			<div class='d-flex flex-wrap justify-content-start col-3'><a class="navbar-brand" href="index.html"
+				><img src="img/logo.png"
+			  /></a>
+			</div>
+			
+		   <!-- Actual search box -->
+		<div class="form-group has-search form-inline md-form col-md-6 col-sm-12">
+			<span class="fa fa-search form-control-feedback"></span>
+			<input type="text" class="form-control col-8 " >
+			<button type="button" class="btn btn-primary btn-lg">
+				SEARCH
+			  </button>
+		</div> 
+	
+			<ul class="d-flex flex-wrap justify-content-end col-md-3 col-sm-12">
+				<li class=" nav-item">
+					<a class="nav-link " href="" ><i class="icon fab fa-facebook-f"></i></a>
+				  </li>
+				  <li class=" nav-item">
+					<a class="nav-link " href="" ><i class="icon fab fa-twitter"></i></a>
+				  </li>
+				  <li class=" nav-item">
+					<a class="nav-link " href="" ><i class="icon fab fa-linkedin-in"></i></a>
+				  </li>
+				<li class=" nav-item">
+				  <a class="nav-link " href="login.html" ><i class="icon fas fa-sign-in-alt"></i>Login</a>
+				</li>
+				<li class=" nav-item">
+				  <a class="nav-link" href="cart.html"><i class="icon fas fa-shopping-cart"></i>Cart</a>
+				</li>
+			<!-- 	<li class=" nav-item">
+				  <a class="nav-link" href="serices.html">Services</a>
+				</li>
+				<li class=" nav-item">
+				  <a class="nav-link" href="team.html">Team</a>
+				</li>
+				<li class=" nav-item">
+				  <a class="nav-link" href="about.html">Connect</a>
+				</li> -->
+			  </ul>	
+			  <hr class="col-12"/>
+		</div>
+		
+       <div class="col-12 ">
+		
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarResponsive"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav">
+            <li class=" nav-item">
+              <a class="nav-link " href="#" >Stationary</a>
+			</li>
+			<li class=" nav-item">
+				<a class="nav-link " href="#" >Electronics</a>
+			  </li>
+			  <li class=" nav-item">
+				<a class="nav-link " href="#" >Computers</a>
+			  </li>
+			  <li class=" nav-item">
+				<a class="nav-link " href="#" >Phones</a>
+			  </li>
+			  <li class=" nav-item">
+				<a class="nav-link " href="#" >Grocery for Business</a>
+			  </li>
+			  <li class=" nav-item">
+				<a class="nav-link " href="#" >Photocopiers & MFD Printers</a>
+			  </li>
+			  <li class=" nav-item">
+				<a class="nav-link " href="#" >Corporate Gift Items</a>
+			  </li>
+			  <li class=" nav-item">
+				<a class="nav-link " href="#" >Promotional Products</a>
+			  </li>
+            <li class=" nav-item">
+              <a class="nav-link" href="#">Sign Up</a>
+            </li>
+            <li class=" nav-item">
+              <a class="nav-link" href="#">Customer Care</a>
+            </li>
+            
+          </ul>
+        </div>
+	  </div>
+	  </div>
+    </nav>
+  <!--- site tag line -->
+<div class='d-flex flex-wrap col-12 bg vision'>
+	<div class='mr-auto pl-4'>
+		<p>We subscribe to our UK affiliate Business Ethics</p>
+		</div>
+		<div >
+		<p>DELIVERING A BETTER TOMORROW TODAY </p>
+		</div>
+		<div class='ml-auto'>
+		</div>
+
+</div>
